b8a931d299d98567aa498679f01b9f6ad1cdd397
James Salas <j@jamesbs.com>
2015-12-23 11:45:53 -0500

    day 5 solution pt2

--- day5/index.ts
+++ day5/index.ts
@@ -7,21 +7,30 @@ readFile(path.join(__dirname, 'input.txt'), 'utf8')
     .then((data: string) => {
         const strings = data.split('\n');
         
-        const niceStrings: number = R.transduce(
-            R.map((s: string) => {
-                const matchVowels = s.match(/[aeiou]/ig);
-                
-                if(   matchVowels !== null && matchVowels.length >= 3
-                   && s.search(/([a-z])\1/i) !== -1
-                   && s.search(/(ab|cd|pq|xy)/i) === -1)
-                    return 1;
-                else
-                    return 0;
-            }),
-            R.add,
-            0,
-            strings.slice(0,-1)
-        );
+        const countStrings: (any) => number = R.partialRight(R.transduce, 
+            [ R.add, 0, strings.slice(0, -1) ]);
+            
+        const niceStringsPt1 = countStrings(R.map((s: string) => {
+            const matchVowels = s.match(/[aeiou]/ig);
+            
+            if (   matchVowels !== null && matchVowels.length >= 3
+                && s.search(/([a-z])\1/i) !== -1
+                && s.search(/(ab|cd|pq|xy)/i) === -1)
+                return 1;
+            else
+                return 0;
+        }));
         
-        console.log(niceStrings);
+        const niceStringsPt2 = countStrings(R.map((s: string) => {
+            if (   s.search(/([a-z]{2}).*\1/i) !== -1
+                && s.search(/([a-z])[a-z]\1/i) !== -1) {
+                    //console.log(s);
+                return 1;
+            }
+            else
+                return 0;
+        }));
+        
+        console.log('part 1: ' + niceStringsPt1);
+        console.log('part 2: ' + niceStringsPt2);
     });

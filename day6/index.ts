5258000491684c6df21151a02293e06211b79d79
James Salas <j@jamesbs.com>
2015-12-23 21:01:24 -0500

    add day6 pt2

--- day6/index.ts
+++ day6/index.ts
@@ -23,48 +23,72 @@ readFile(path.join(__dirname, 'input.txt'), 'utf8')
         const instructions = data.split('\n').slice(0, -1);
         const enclosingBounds = { start: { x: 0, y: 0 }, end: { x: 0, y: 0 } };
         
-        const actions = {
-            'turn on': (current: boolean) => true,
-            'turn off': (current: boolean) => false,
-            'toggle': (current: boolean) => !current
-        };
-        
         const makePoint = (coordinates: string) : Point => {
             const [x, y] = coordinates.split(',').map((v: string) => parseInt(v, 10));
             return { x: x, y: y };
         };
         
-        const setLights = (action: LightAction, bounds: Bounds, lights) => {
+        function setLights(actionSet, instruction: string, bounds: Bounds, lights) {
+            const action = actionSet[instruction];
+            
             R.range(bounds.start.x, bounds.end.x + 1)
                 .forEach((x: number) => {
                     R.range(bounds.start.y, bounds.end.y + 1).forEach((y: number) => {
-                        //console.log(`(${x},${y}) ${lights[x][y]} => ${action(lights[x][y])}`);
                         lights[x][y] = action(lights[x][y]);
                     }); 
                 });
            return lights;
         };
         
-        const transducer = R.map(R.compose(
+        const setLightsPt1 = R.partial(setLights, []);
+        const getComponents = (instruction: string) : string[] => instruction.split(/ (\d*,?\d+)/).slice(0, -1);
+        
+        
+        const lightField = R.transduce(
+            R.map(R.compose(
             (components: string[]) => {
                 // console.log(components, components[0], "true =>", actions[components[0]](true));
-                return R.partial(setLights, [ 
-                    actions[components[0]],
+                return R.partial(setLights, [
+                    {
+                        'turn on': (current: boolean) => true,
+                        'turn off': (current: boolean) => false,
+                        'toggle': (current: boolean) => !current
+                    },
+                    components[0],
                     { start: makePoint(components[1]), end: makePoint(components[3]) }
                 ]);
             },
-            (instruction: string) : string[] => instruction.split(/ (\d*,?\d+)/).slice(0, -1)
-        ));
-        
-        const lightField = R.transduce(
-            transducer,
-            (lights, modifier) => {
-                return modifier(lights)
-            },
+            getComponents)),
+            (lights, modifier) => { return modifier(lights) },
             R.range(0, 1000).map(x => R.range(0, 1000).map(y => false)),
             instructions
         );
         
         const areOn: number = R.flatten(lightField).filter((isOn: boolean) => isOn).length;
-        console.log(areOn);
+        
+        console.log('part 1: ' + areOn);
+        
+                
+        const brightField = R.transduce(
+            R.map(R.compose(
+            (components: string[]) => {
+                return R.partial(setLights, [
+                    {
+                        'turn on': (current: number) => current + 1,
+                        'turn off': (current: number) => Math.max(0, current - 1),
+                        'toggle': (current: number) => current + 2
+                    },
+                    components[0],
+                    { start: makePoint(components[1]), end: makePoint(components[3]) }
+                ]);
+            },
+            getComponents)),
+            (lights, modifier) => { return modifier(lights) },
+            R.range(0, 1000).map(x => R.range(0, 1000).map(y => 0)),
+            instructions
+        );
+        
+        const totalBrightness: number = R.flatten(brightField).reduce(R.add);
+            
+       console.log('part 2:', totalBrightness);
     });
